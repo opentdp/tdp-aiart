f354cf40ba4387925cd4b0c1b05c484d157064ac
若海 <wang@rehiy.com>
2023-03-30 21:05:03 +0800

    perf: 优化逻辑

--- factory/api/aiart/controller.go
+++ factory/api/aiart/controller.go
@@ -1,31 +1,16 @@
 package aiart
 
 import (
-	"strings"
-	"tdp-aiart/cmd/args"
-	"tdp-aiart/helper/tencent"
-	"tdp-aiart/module/model/artimg"
-
 	"github.com/gin-gonic/gin"
-	"github.com/mitchellh/mapstructure"
 )
 
 func create(c *gin.Context) {
 
 	// 构造参数
 
-	param := &tencent.ReqeustParam{}
+	param := &ReqeustParams{}
 
 	if err := c.ShouldBindJSON(param); err != nil {
-		c.Set("Error", err)
-		return
-	}
-
-	// 读取参数
-
-	payload := ToImageRequest{}
-
-	if err := mapstructure.Decode(param.Payload, &payload); err != nil {
 		c.Set("Error", err)
 		return
 	}
@@ -39,37 +24,12 @@ func create(c *gin.Context) {
 		return
 	}
 
-	// 保存图片
+	// 存储数据
 
-	outputP, _ := saveOutputImage(res)
-
-	if payload.InputImage != "" {
-		inputP := outputP + "-input.png"
-		base64 := strings.Split(payload.InputImage, ",")[1]
-		if saveBase64ImageToFile(base64, inputP) == nil {
-			payload.InputImage = strings.ReplaceAll(inputP, args.Dataset.Dir, "")
-		}
-	}
-
-	artimg.Create(&artimg.CreateParam{
-		Subject:    payload,
-		OutputFile: strings.ReplaceAll(outputP, args.Dataset.Dir, ""),
-	})
+	saveObject(&param.Payload, res)
 
 	// 输出数据
 
 	c.Set("Payload", res)
 
-}
-
-type ToImageRequest struct {
-	InputImage     string
-	InputUrl       string
-	Prompt         string
-	NegativePrompt string
-	Styles         []string
-	ResultConfig   any
-	LogoAdd        int64
-	LogoParam      any
-	Strength       float64
 }
---
+++ factory/api/aiart/provider.go
@@ -0,0 +1,68 @@
+package aiart
+
+import (
+	"errors"
+
+	"tdp-aiart/helper/tencent"
+	"tdp-aiart/module/model/config"
+
+	"github.com/mitchellh/mapstructure"
+)
+
+func apiProxy(rq *ReqeustParams) (*ToImageResponse, error) {
+
+	// 获取密钥
+
+	kv := config.ValuesOf("Tencent")
+
+	if kv["secretId"] == "" || kv["secretKey"] == "" {
+		return nil, errors.New("请先配置腾讯云密钥")
+	}
+
+	// 发起请求
+
+	param := &tencent.ReqeustParam{
+		Service:   "aiart",
+		Region:    "ap-guangzhou",
+		Version:   "2022-12-29",
+		SecretId:  kv["secretId"],
+		SecretKey: kv["secretKey"],
+		Action:    rq.Action,
+		Payload:   rq.Payload,
+	}
+
+	resp, err := tencent.Request(param)
+
+	// 解析参数
+
+	output := &ToImageResponse{}
+
+	if err == nil {
+		err = mapstructure.Decode(resp, &output)
+	}
+
+	return output, err
+
+}
+
+type ReqeustParams struct {
+	Action  string
+	Payload ToImageRequest
+}
+
+type ToImageRequest struct {
+	InputImage     string
+	InputUrl       string
+	Prompt         string
+	NegativePrompt string
+	Styles         []string
+	ResultConfig   any
+	LogoAdd        int64
+	LogoParam      any
+	Strength       float64
+}
+
+type ToImageResponse struct {
+	RequestId   string
+	ResultImage string
+}
