4227aa162592b5a0288ccb7c8d3476b02bd39536
若海 <wang@rehiy.com>
2023-03-31 10:27:32 +0800

    style: 清理冗余代码

--- frontend/src/apps/dashboard/index.vue
+++ frontend/src/apps/dashboard/index.vue
@@ -1,17 +1,8 @@
 <script lang="ts">
 import { Component, Vue } from "vue-facing-decorator"
 
-import sessionStore from "@/store/session"
-
 @Component
 export default class DashboardIndex extends Vue {
-    public session = sessionStore()
-
-    // 初始化
-
-    public created() {
-        this.session.Level = 1
-    }
 }
 </script>
 
