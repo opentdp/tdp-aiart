4c747d8ca65a0beb4c1f5dce0e3b86f56ff5fe98
若海 <wang@rehiy.com>
2023-04-12 13:51:43 +0800

    chore: 添加基础工具

---
+++ build.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+#
+
+set -e
+set -o noglob
+
+###########################################
+
+export CGO_ENABLED=0
+export GO111MODULE=on
+
+build() {
+    echo building for $1/$2
+    out=build/tdp-aiart-$1-$2$3
+    GOOS=$1 GOARCH=$2 go build -ldflags="-s -w" -o $out main.go
+}
+
+####################################################################
+
+releases_url=https://api.github.com/repos/open-tdp/tdp-aiart-ui/releases/latest
+download_url=`wget -qO- $releases_url | grep releases/download | cut -f4 -d "\""`
+
+wget -O aiart-ui.tar.gz $download_url
+tar xvf aiart-ui.tar.gz --strip-components 2 -C front
+
+####################################################################
+
+build android arm64
+
+build darwin amd64
+build darwin arm64
+
+build linux amd64
+build linux arm64
+
+build windows amd64 .exe
+build windows arm64 .exe
+
+####################################################################
+
+for app in `ls build`; do
+    gzip build/$app
+done
