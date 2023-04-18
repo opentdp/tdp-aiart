f7b81c1a8c625af709624ee3271e8cfbe8082901
若海 <wang@rehiy.com>
2023-04-18 15:13:44 +0800

    docs: 更新帮助信息

--- cmd/args/build.go
+++ cmd/args/build.go
@@ -8,7 +8,9 @@ const AppSummary = "土豆片智能绘画"
 
 const UpdateUrl = "https://aiart.opentdp.org/update"
 
-const ReadmeText = `土豆片控制面板
+const ReadmeText = AppSummary + `
 
-开源项目 https://github.com/open-tdp/tdp-aiart
-问题提交 https://github.com/open-tdp/tdp-aiart/issues`
+开源智能绘画分享平台
+
+TDP Website: https://www.opentdp.org
+Open Source: https://github.com/open-tdp`
