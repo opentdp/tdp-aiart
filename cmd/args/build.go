5ce1ea0da0d7d39c4aa30b7d16ccd05df3e78e03
若海 <wang@rehiy.com>
2023-04-18 22:55:00 +0800

    release v0.5.0

--- cmd/args/build.go
+++ cmd/args/build.go
@@ -1,7 +1,7 @@
 package args
 
-const Version = "0.4.2"
-const BuildVersion = "230409"
+const Version = "0.5.0"
+const BuildVersion = "230410"
 
 const AppName = "TDP Aiart"
 const AppSummary = "土豆片智能绘画"
