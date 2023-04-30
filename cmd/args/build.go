074cc3c67ebc89bf0191e918331c771039148a5e
若海 <wang@rehiy.com>
2023-04-30 16:52:32 +0800

    release v0.6.0

--- cmd/args/build.go
+++ cmd/args/build.go
@@ -1,16 +1,16 @@
 package args
 
-const Version = "0.5.1"
-const BuildVersion = "230411"
+const Version = "0.6.0"
+const BuildVersion = "230412"
 
 const AppName = "TDP Aiart"
-const AppSummary = "土豆片智能绘画"
+const AppSummary = "土豆片AI艺术"
 
 const UpdateUrl = "https://aiart.opentdp.org/update/"
 
 const ReadmeText = AppSummary + `
 
-开源智能绘画分享平台
+开源AI艺术分享平台
 
 TDP Website: https://www.opentdp.org
 Open Source: https://github.com/open-tdp`
